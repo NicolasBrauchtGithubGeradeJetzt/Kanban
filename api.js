9626bb112e5c64c114453de363cbcb237242b898
BBZW-Sursee Meili Simon <Simon_Meili@sluz.ch>
2020-11-25 09:30:08 +0100

    implemented delete and post

--- api.js
+++ api.js
@@ -2,13 +2,31 @@
 import{Application,Router,send}from"https://deno.land/x/oak@v6.3.1/mod.ts";
 const app=new Application();
 const router=new Router();
-let kanban = [];
+let items = [];
+let id = 0;
 router
     .get("/kanban/columns",context=>context.response.body={
         "id":"Todo",
         "columns":["ToDo","In progress", "Done"]
     }
-    );
+    )
+    .post("/kanban/createitem",async context =>{
+        const item = await context.request.body({ type: "json" }).value;
+        item.id =id;
+        items =[
+            ...items,
+            item];
+        id++;
+
+    }
+    )
+    .get("/kanban/items",context=>context.response.body= items
+
+    )
+    .delete("/kanban/delete/:id", context => {
+        const id = context.params.id;
+        items = items.filter(i => i.id != id);
+    });
 app.use(router.routes());
 app.use(async context => {
     await send(context, context.request.url.pathname, {
