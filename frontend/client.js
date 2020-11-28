acb272ff788a659671e420f1fd9cd3811b6215e4
NicolasBrauchtGithubGeradeJetzt <NicholasAlex_Kingsman@sluz.ch>
2020-11-28 12:54:05 +0100

    Created Function-structure + Update_items added beta-functionality

--- frontend/client.js
+++ frontend/client.js
@@ -1,4 +1,5 @@
 console.log("client works");
+
 async function startup(){
     try{
         const response = await fetch('http://localhost:8000/kanban/columns');
@@ -12,7 +13,57 @@ async function startup(){
             result += '</tr></thead><tbody></tbody></table>';
             main.innerHTML = result;
         }
+        update_items();
         
+    }catch(errorReason){
+        console.error(errorReason)
+    }
+}
+
+async function add_item(){
+    try{
+        update_items();
+    }catch(errorReason){
+        console.error(errorReason)
+    }
+}
+
+async function delete_item(){
+    try{
+        update_items();
+    }catch(errorReason){
+        console.error(errorReason)
+    }
+}
+
+async function update_items(){
+    try{
+        const table_length = document.querySelectorAll('th').length;
+        console.log(table_length);
+        const tbody = document.querySelector('tbody');
+        let result = '';
+
+        const response = await fetch('http://localhost:8000/kanban/items');
+        const items = await response.json();
+        console.log(items);
+
+        let items_ordered = [];
+        let position_array_tracker = [];
+        
+        if(items.length != 0){
+
+        }else{
+            result += '<tr>';
+            for(pos = 0;pos != table_length; pos++){
+                result += '<td><button>+</button></td>';
+            }
+            result += '</tr>';
+        }
+
+        tbody.innerHTML = result;
+
+        //tbody.innerHTML = '<tr><td>+</td><td>+</td><td>+</td></tr>'
+
     }catch(errorReason){
         console.error(errorReason)
     }
