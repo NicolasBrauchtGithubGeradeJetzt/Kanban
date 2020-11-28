b23d2a9770905dddccab8e037f3adee3c99b66b9
NicolasBrauchtGithubGeradeJetzt <NicholasAlex_Kingsman@sluz.ch>
2020-11-28 18:18:52 +0100

    Added functionailty "add_item" + refined functionality "update_items)

--- frontend/client.js
+++ frontend/client.js
@@ -20,8 +20,18 @@ async function startup(){
     }
 }
 
-async function add_item(){
+async function add_item(pos){
     try{
+        const response = await fetch('http://localhost:8000/kanban/createitem', {
+            method: 'POST',
+            body: JSON.stringify({
+                position: pos,
+                title: 'placeholder ' + Math.random(),
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
         update_items();
     }catch(errorReason){
         console.error(errorReason)
@@ -39,31 +49,50 @@ async function delete_item(){
 async function update_items(){
     try{
         const table_length = document.querySelectorAll('th').length;
-        console.log(table_length);
         const tbody = document.querySelector('tbody');
+        let row_length = 0;
         let result = '';
 
         const response = await fetch('http://localhost:8000/kanban/items');
         const items = await response.json();
-        console.log(items);
+        console.log("items",items);
 
+        //Ordere Items, to implement to table-body
         let items_ordered = [];
-        let position_array_tracker = [];
-        
-        if(items.length != 0){
+        for(var pos = 0; pos != table_length; pos++){
+            items_ordered[pos] = items.filter(item => item.position == pos);
+            items_ordered[pos] = [...items_ordered[pos], '+'];
+            if(row_length < items_ordered[pos].length){
+                row_length = items_ordered[pos].length;
+            }
+        }
 
-        }else{
+        //Implementing Data to table
+        for(var row = 0; row != row_length; row++){
             result += '<tr>';
-            for(pos = 0;pos != table_length; pos++){
-                result += '<td><button>+</button></td>';
+            for(var pos = 0; pos != table_length; pos++){
+                result += '<td>';
+                if(items_ordered[pos].length > row){
+                    let item = items_ordered[pos][row];
+                    if(item == '+'){
+                        result += '<button id="' + pos + '">+</button>';
+                    }else{
+                        result += '<article class="' + item.id + '">' + item.title + '</article>';
+                    }
+                }
+                result += '</td>';
             }
             result += '</tr>';
         }
 
         tbody.innerHTML = result;
 
-        //tbody.innerHTML = '<tr><td>+</td><td>+</td><td>+</td></tr>'
-
+        const buttons = document.querySelectorAll('button');
+        buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                add_item(button.id);
+            });
+        })
     }catch(errorReason){
         console.error(errorReason)
     }
